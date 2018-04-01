aeb67ab0a4910493286b9166b7bc3a0d928d8ed8
sbujarski <s.bujarski@gmail.com>
2018-03-31 23:43:55 -0700

    Initializing R Code

---
+++ Translational-MetaAnalysis R Code.R
@@ -0,0 +1,6 @@
+#Translational Meta-Analysis R Code
+#Goals of this study are to 
+# 1) Collect data on all published trials using either:
+#   A) Randomized Controlled Trial of a medication for an Alcohol Use Disorder (AUD)
+#   B) Laboratory study testing the effects of a medication on responses to alcohol administration
+# 2) Test the concordance between these different outcomes across the ranges of medications tested. 
